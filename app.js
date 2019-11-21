d2f5e4c29ff919b19b67cc718e98180371ab16d9
johnny <>
2019-11-21 11:14:39 +0900

    no message

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-(function($) {
-    var $nav   = $('#navArea');
-    var $btn   = $('.toggle_btn');
-    var $mask  = $('#mask');
-    var open   = 'open'; // class
-    // menu open close
-    $btn.on( 'click', function() {
-      if ( ! $nav.hasClass( open ) ) {
-        $nav.addClass( open );
-      } else {
-        $nav.removeClass( open );
-      }
+function toggleNav() {
+    var body = document.body;
+    var hamburger = document.getElementById('js-hamburger');
+    var blackBg = document.getElementById('js-black-bg');
+  
+    hamburger.addEventListener('click', function() {
+      body.classList.toggle('nav-open');
     });
-    // mask close
-    $mask.on('click', function() {
-      $nav.removeClass( open );
+    blackBg.addEventListener('click', function() {
+      body.classList.remove('nav-open');
     });
-  } )(jQuery);
+  }
+  toggleNav();
